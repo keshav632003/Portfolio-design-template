6ccf76291c48ac33fcb7e6fee24197a8f26dd03a
pulkitgupta30 <76155456+pulkitgupta30@users.noreply.github.com>
2021-01-24 18:15:58 +0530

    Add files via upload
    portfolio

---
+++ pulu.js
@@ -0,0 +1,37 @@
+const sr = ScrollReveal({
+    // delay: 200,
+    origin: "left",
+    distance: "500px",
+    duration: 1000,
+    reset: true,
+});
+sr.reveal("#img1", {
+    // origin: top,
+    delay: 200
+})
+sr.reveal("#heading", {
+    origin: top,
+    delay: 200
+})
+sr.reveal(".second", {
+    interval: 200,
+    delay: 200
+});
+sr.reveal("#Pulkit", {
+    // origin: top,
+    delay: 400,
+})
+// sr.reveal("#hacker", {
+//     origin: top,
+//     delay: 200
+// })
+sr.reveal(".social", {
+
+    interval: 400,
+    delay: 400
+});
+sr.reveal(".flex", {
+
+    interval: 400,
+    delay: 400
+});
